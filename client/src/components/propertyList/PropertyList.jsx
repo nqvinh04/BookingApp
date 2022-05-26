5002bfbed1c480bd1b291314cb5f59815fe15ac2
Vinh Nguyen (BnK - HN) <vinh.nguyen@bnksolution.com>
2022-05-26 08:51:13 +0700

    update feature-00001

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,5 +1,6 @@
 import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
+import {Link} from "react-router-dom";
 
 const PropertyList = () => {
     const { data, loading, error } = useFetch("/hotels/countByType");
@@ -20,11 +21,13 @@ const PropertyList = () => {
                     {data &&
                         images.map((img,i) => (
                             <div className="pListItem" key={i}>
-                                <img
-                                    src={img}
-                                    alt=""
-                                    className="pListImg"
-                                />
+                                <Link to="/hotels">
+                                    <img
+                                        src={img}
+                                        alt=""
+                                        className="pListImg"
+                                    />
+                                </Link>
                                 <div className="pListTitles">
                                     <h1>{data[i]?.type}</h1>
                                     <h2>{data[i]?.count} {data[i]?.type}</h2>
